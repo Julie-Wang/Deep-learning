f0c40bd774715f74a20f72aca28c30b543bc0f8d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-10-14 22:39:00 -0600

    Fix timeout issues

--- .tests/nbrun.py
+++ .tests/nbrun.py
@@ -18,7 +18,7 @@ def run_notebook(in_filepath):
 
     nb = nbformat.read(in_filepath.open(), as_version=4)
     ep = ExecutePreprocessor(
-        timeout=300, kernel_name='python%d' % sys.version_info[0])
+        timeout=100000, kernel_name='python%d' % sys.version_info[0])
 
     try:
         ep.preprocess(nb, {'metadata': {'path': run_path}})
